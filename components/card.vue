4cb132348e8b917ed1d7584e2a2f3fa93f0346c0
gaelMon <gael.monbrault@ovianet.com>
2022-03-02 10:10:13 +0100

    add card component

---
+++ components/card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card card-side bg-base-100 shadow-xl">
+    <figure>
+      <img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ title }}</h2>
+      <p>{{ content }}</p>
+      <div class="justify-end card-actions">
+        <button class="btn btn-primary">Lire</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
